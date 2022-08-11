e10957b06b3f1961bf93dc270e02e697bdd71d63
kynius <gadzinakrzys@gmail.com>
2022-08-11 14:15:30 +0200

    Adding MatchRequest, that dont have a time check

--- lolAPI/Interfaces/IRequestsRepo.cs
+++ lolAPI/Interfaces/IRequestsRepo.cs
@@ -3,4 +3,5 @@ namespace lolAPI.Interfaces;
 public interface IRequestsRepo
 {
     public Task<string?> GetRequest(string baseUrl, string requestUrl);
+    public Task<string?> GetMatchRequest(string baseUrl, string requestUrl);
 }
